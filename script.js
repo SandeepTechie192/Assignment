c224e23ce0d1cb58a99984a03aab7dee5e885ee7
sandeep_192 <92150106+sandy62@users.noreply.github.com>
2024-04-28 14:36:44 +0530

    First Push

---
+++ script.js
@@ -0,0 +1,10 @@
+// Nav Active Link Color change with bootstrap
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+    });
+    link.classList.add("active");
+  });
+});
